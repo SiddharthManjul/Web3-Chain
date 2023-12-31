b4786ea5f101b8b4878daca908af60c178568bda
SiddharthManjul <manjulsiddharth09@gmail.com>
2023-12-31 18:37:33 +0530

    Write functions

---
+++ EtherJS/client/src/App.js
@@ -0,0 +1,115 @@
+import "./App.css";
+import { useEffect } from "react";
+const { ethers } = require("ethers");
+
+function App() {
+  const walletAddress = "0xee4a5d5ee49a3f32620c9564cf79f6438033c2b1";
+
+  useEffect(() => {
+    const walletAbi = [
+      {
+        inputs: [],
+        name: "sendEthContract",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "address",
+            name: "_user",
+            type: "address",
+          },
+        ],
+        name: "sendEthUser",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "uint256",
+            name: "_num",
+            type: "uint256",
+          },
+        ],
+        name: "setValue",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "address",
+            name: "_address",
+            type: "address",
+          },
+        ],
+        name: "accountBalance",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "contractBalance",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "getValue",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "name",
+        outputs: [
+          {
+            internalType: "string",
+            name: "",
+            type: "string",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+    ];
+    const writeContract = async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(walletAddress, walletAbi, signer);
+      await contract.setValue(2);
+    };
+    writeContract();
+  }, []);
+
+  return <div className="App">̦</div>;
+}
+
+export default App;
--- EtherJS/interactions.js
+++ EtherJS/interactions.js
@@ -1,124 +1,120 @@
 const { ethers } = require("ethers");
 const provider = new ethers.providers.JsonRpcProvider(
-  `https://eth-mainnet.g.alchemy.com/v2/_hDbJee5R3PBoRVfGNY8ynYq4mZekxQP`
+  `https://eth-sepolia.g.alchemy.com/v2/UEWeBqCKv_CCuHyvSLNqi_ndoJ_xmIm6`
 );
 
-const walletAddress = "0x0146261BB9C5b21403a60D30e5271e1d172B27b9";
+const walletAddress = "0xEE4A5d5ee49A3F32620C9564CF79f6438033c2B1";
 const walletAbi = [
-  {
-    inputs: [],
-    name: "sendEthContract",
-    outputs: [],
-    stateMutability: "payable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_user",
-        type: "address",
-      },
-    ],
-    name: "sendEthUser",
-    outputs: [],
-    stateMutability: "payable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "_num",
-        type: "uint256",
-      },
-    ],
-    name: "setValue",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_address",
-        type: "address",
-      },
-    ],
-    name: "accountBalance",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "contractBalance",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getValue",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "name",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-];
+	{
+		"inputs": [],
+		"name": "sendEthContract",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_user",
+				"type": "address"
+			}
+		],
+		"name": "sendEthUser",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_num",
+				"type": "uint256"
+			}
+		],
+		"name": "setValue",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_address",
+				"type": "address"
+			}
+		],
+		"name": "accountBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "contractBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getValue",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
 
-const contractIntreaction = async () => {
-  const walletContract = new ethers.Contract(
-    walletAddress,
-    walletAbi,
-    provider
-  );
+const contractInteraction = async() => {
+	const walletContract = new ethers.Contract(walletAddress, walletAbi, provider);
 
-  // const contractName = await walletContract.name();
-  // console.log("Contract Name:", contractName);
+	const contractName = await walletContract.name();
+	console.log("Contract name:",contractName);
 
-  // const num = await walletContract.getValue();
-  // console.log("Number Value:", String(num));
+	const num = await walletContract.getValue();
+	const numInEther = ethers.utils.formatEther(num);
+	console.log("Number Value:", String(num)); // String(num) can be used aswell in BigNumber
 
-  const contractBalance = await walletContract.contractBalance();
-  const balethContract = ethers.utils.formatEther(contractBalance);
-  console.log("Contract Balance:", balethContract);
+	const contractBalance = await walletContract.contractBalance();
+	const contractBalanceInEther = ethers.utils.formatEther(contractBalance);
+	console.log("Balance:", contractBalanceInEther);
 
-  const userBalance = await walletContract.accountBalance(
-    "0x80badfb970416bef0241cf924e708a6526dd2e60"
-  );
-  const balethUser = ethers.utils.formatEther(userBalance);
-  console.log("User Balance:", balethUser);
-};
-contractIntreaction();
+	const accountBalance = await walletContract.accountBalance("0x0146261BB9C5b21403a60D30e5271e1d172B27b9");
+	const balanceInEther = ethers.utils.formatEther(accountBalance);
+	console.log("User Balance:", balanceInEther);
+}
+
+contractInteraction();
