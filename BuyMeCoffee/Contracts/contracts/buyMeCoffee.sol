7102a51faab0b675074533b6d7a4c04411862378
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-01-17 16:25:48 +0530

    Deploy Script Completed

--- BuyMeCoffee/Contracts/contracts/buyMeCoffee.sol
+++ BuyMeCoffee/Contracts/contracts/buyMeCoffee.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+// Deployed to Sepolia at 0xbc66A8B4c48867E71c6Ef7A8Fe8c129cFcB62d0A
+
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
